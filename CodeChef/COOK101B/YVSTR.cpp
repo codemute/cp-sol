b069856bd969acdcff2d0f1346949031131a2696
Abhinav Kaushlya <abhinavkaushlya@gmail.com>
2019-07-11 21:54:28 +0530

    init

---
+++ CodeChef/COOK101B/YVSTR.cpp
@@ -0,0 +1,67 @@
+// Copyright 2019 abhishalya
+
+#include <bits/stdc++.h>
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::pair;
+using std::max;
+using std::make_pair;
+
+// Was not very simple, lot of thinking involved here.
+// The editorial is good and easy to understand.
+
+int main() {
+  int t; cin >> t;
+  while (t--) {
+    int n; cin >> n;
+    string s; cin >> s;
+    int a = 0, cnt = 1; char ca;
+    vector<pair<int, int> > v[26][26];
+    int sing[26]; memset(sing, 0, sizeof sing);
+    for (int i = 1; i < n; i++) {
+      if (s[i] == s[i - 1]) {
+        cnt++;
+      } else {
+        sing[s[i - 1] - 'a'] = max(sing[s[i - 1] - 'a'], cnt);
+        if (a == 0) {
+          ca = s[i - 1];
+          a = cnt;
+          cnt = 1;
+        } else {
+          v[ca - 'a'][s[i - 1] - 'a'].push_back(make_pair(a, cnt));
+          ca = s[i - 1]; a = cnt; cnt = 1;
+        }
+      }
+    }
+    sing[s[n - 1] - 'a'] = max(sing[s[n - 1] - 'a'], cnt);
+    if (a != 0) {
+      v[ca - 'a'][s[n - 1] - 'a'].push_back(make_pair(a, cnt));
+    }
+    int64_t ans = 0; int las = 0;
+    for (int i = 0; i < 26; i++) {
+      ans += sing[i];
+      for (int j = 0; j < 26; j++) {
+        vector<pair<int, int> > vec; las = 0;
+        sort(v[i][j].begin(), v[i][j].end());
+        // cout << i << " " << j << " " << v[i][j].size() << endl;
+        for (auto k : v[i][j]) {
+          while (!vec.empty() && vec.back().second <= k.second) {
+            vec.pop_back();
+          }
+          vec.push_back(k);
+        }
+        for (auto k : vec) {
+          // cout << "k = " << k.first << " " << las << " " << k.second << endl;
+          ans += 1LL * (k.first - las) * k.second;
+          las = k.first;
+        }
+        // cout << "ans = " << ans << endl;
+      }
+    }
+    cout << ans << endl;
+  }
+  return 0;
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+[![Build Status][build]][travis]
+
+# Competitive Programming - Solutions
+
+We know that there are lots of contests happening every month, probably every
+week on various sites like Codeforces, CodeChef, Hackerrank, Hackerearth, etc.
+Phew! There are lot of sites. We know sometimes you just need the solution to
+some particular problem you had previously solved, or maybe you want to have a
+quick look at the solution just to remind yourselves how you had solved it.
+
+Well, this repository will be helpful to you then. Here are the solutions to 
+every problem we solved across different sites so you can have a look at them.
+Beware that all of these solutions are **only updated after the contest has**
+**ended**. We too disgust the idea of plagiarism.
+
+If you find any mistakes in the solution, make sure to [create an issue][issue].
+Also, If you find this repository useful make sure to give it a star. :star:
+
+[travis]: https://travis-ci.org/codemute/cp-sol
+[build]: https://travis-ci.org/codemute/cp-sol.svg?branch=master
+[issue]: https://github.com/codemute/cp-sol/issues/new
